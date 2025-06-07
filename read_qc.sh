ff4d80dd530c9cb890b8b59c85da5a855d548df4
Mahnaz Azad <azad.mahnaz228@gmail.com>
2025-06-07 16:42:27 -0400

    multi fastqc on trimmed reads

--- read_qc.sh
+++ read_qc.sh
@@ -14,22 +14,35 @@
 
 #Runs Trimmomatic in paired-end mode (PE) to trim adapters and filter out low-quality reads.
 
-module load Trimmomatic/0.39-Java-13
-TRIMMOMATIC="java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar"
+#module load Trimmomatic/0.39-Java-13
+#TRIMMOMATIC="java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar"
 
-mkdir data/trimmed_fastq
+#mkdir data/trimmed_fastq
 
-for fwd in data/raw_fastq/*_1.fastq.gz
-do
-    sample=$(basename $fwd _1.fastq.gz)
-    rev="data/raw_fastq/${sample}_2.fastq.gz"
+#for fwd in data/raw_fastq/*_1.fastq.gz
+#do
+ #   sample=$(basename $fwd _1.fastq.gz)
+  #  rev="data/raw_fastq/${sample}_2.fastq.gz"
 
-    echo "Processing sample: $sample"
+   # echo "Processing sample: $sample"
+#
+ #   $TRIMMOMATIC PE \
+  #  data/raw_fastq/${sample}_1.fastq.gz data/raw_fastq/${sample}_2.fastq.gz \
+   # data/trimmed_fastq/${sample}_1.paired.fastq.gz data/trimmed_fastq/${sample}_1.unpaired.fastq.gz \
+    #data/trimmed_fastq/${sample}_2.paired.fastq.gz data/trimmed_fastq/${sample}_2.unpaired.fastq.gz \
+    #ILLUMINACLIP:data/raw_fastq/NexteraPE-PE.fa:2:30:10 \
+    #LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+#done
 
-    $TRIMMOMATIC PE \
-    data/raw_fastq/${sample}_1.fastq.gz data/raw_fastq/${sample}_2.fastq.gz \
-    data/trimmed_fastq/${sample}_1.paired.fastq.gz data/trimmed_fastq/${sample}_1.unpaired.fastq.gz \
-    data/trimmed_fastq/${sample}_2.paired.fastq.gz data/trimmed_fastq/${sample}_2.unpaired.fastq.gz \
-    ILLUMINACLIP:data/raw_fastq/NexteraPE-PE.fa:2:30:10 \
-    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-done
+
+#Run fastqc on my trimmed reads
+
+# Run the fastqc on individual raw data
+#module load FastQC/0.11.9-Java-11
+#mkdir data/fastqc_trimmed_results
+#fastqc -o data/fastqc_trimmed_results data/trimmed_fastq/*.fastq.gz
+
+#Run Multiqc to compile the trimmed FASTqc results
+module load MultiQC/1.14-foss-2022a
+mkdir data/multi_fastqc_trimmed_results
+multiqc -o data/multi_fastqc_trimmed_results data/fastqc_trimmed_results
